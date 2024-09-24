ad81df3528ee04c958451ec9d75515ad8f55be94
LucasKMS <lucas.sampaio9@fatec.sp.gov.br>
2024-09-23 22:16:41 -0300

    Add Validation and edit Page for Products

---
+++ src/components/templates/products/EditTemplate.tsx
@@ -0,0 +1,91 @@
+"use client";
+
+import { TextField, MenuItem, Select, Button, Box } from "@mui/material";
+import React from "react";
+import Layout from "@/components/UI/organisms/Layout";
+import { useFormik } from "formik";
+import { IProduct } from "@/interfaces/IProduct";
+import { ProductEditValidator } from "@/validators/ProductEditValidator";
+
+const EditTemplate: React.FC = () => {
+  const formik = useFormik<IProduct>({
+    initialValues: {
+      description: "",
+      brand: "",
+      value: 0,
+      weight: 0,
+      flavor: "",
+    },
+    validationSchema: ProductEditValidator,
+
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
+  const { handleSubmit, values, handleChange, setFieldValue, errors } = formik;
+
+  return (
+    <Layout>
+      <Box component="form" onSubmit={handleSubmit}>
+        <TextField
+          name="description"
+          label="Descrição"
+          fullWidth
+          value={values.description}
+          onChange={handleChange}
+          error={!!errors.description}
+          helperText={errors.description}
+        />
+        <TextField
+          name="brand"
+          label="Marca"
+          fullWidth
+          value={values.brand}
+          onChange={handleChange}
+          error={!!errors.description}
+          helperText={errors.description}
+        />
+        <TextField
+          name="value"
+          label="Valor"
+          fullWidth
+          value={values.value}
+          onChange={handleChange}
+          error={!!errors.description}
+          helperText={errors.description}
+        />
+        <TextField
+          name="weight"
+          label="Peso (gr.)"
+          fullWidth
+          value={values.weight}
+          onChange={handleChange}
+          error={!!errors.description}
+          helperText={errors.description}
+        />
+        <Select
+          name="flavor"
+          label="Sabor"
+          fullWidth
+          value={values.flavor}
+          onChange={(e) => setFieldValue("flavor", e.target.value)}
+        >
+          <MenuItem value=""> --Não Informado-- </MenuItem>
+          <MenuItem value="morango">Morango</MenuItem>
+          <MenuItem value="chocolate">Chocolate</MenuItem>
+          <MenuItem value="abacaxi">Abacaxi</MenuItem>
+        </Select>
+        <Button variant="outlined" color="secondary">
+          Cancelar
+        </Button>
+
+        <Button variant="contained" color="primary" type="submit">
+          Atualizar
+        </Button>
+      </Box>
+    </Layout>
+  );
+};
+
+export default EditTemplate;
---
+++ src/validators/ProductEditValidator.ts
@@ -0,0 +1,13 @@
+import { describe } from 'node:test';
+import * as Yup from 'yup';
+
+export const ProductEditValidator = () => {
+    return Yup.object().shape({
+        description: Yup.string().required("Campo Obrigatório!").min(3, "Mínimo de ${min} caracteres")
+        .max(100, "Mínimo de ${min} caracteres"),
+        brand: Yup.string().required("Campo Obrigatório!").max(80, "Mínimo de ${min} caracteres"),
+        value: Yup.number().min(0.01, "Mínimo de ${min} caracteres").required("Campo Obrigatório!"),
+        weight: Yup.number().min(0.01, "Mínimo de ${min} caracteres"),
+        flavor: Yup.string(),
+    });
+}
